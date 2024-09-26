9357cf95c6ad9a2a4f501e708156b8eb68c97cda
seungdeok <akk9132@naver.com>
2024-09-27 07:58:46 +0900

    chore: configure babel

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: ["@babel/preset-react"],
+  plugins: [
+    "@babel/plugin-transform-template-literals",
+    "@babel/plugin-transform-arrow-functions",
+  ],
+};
---
+++ src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>REACT JS APP</h1>;
+}
+const root = document.getElementById("root");
+ReactDOM.render(<App />, root);
